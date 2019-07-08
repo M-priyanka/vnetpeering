0b8e2760927dfc040aebc6adefe6b6c0446158f2
M-priyanka <31058016+M-priyanka@users.noreply.github.com>
2019-07-08 15:34:57 +0530

    Create sample.sh

---
+++ sample.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /home/$USER
+echo $1 >> testlog.txt
+echo $2 >> testlog.txt
+echo $3 >> testlog.txt
+echo $4 >> testlog.txt
+echo $5 >> testlog.txt
+echo $6 >> testlog.txt
+echo $7 >> testlog.txt
+echo $8 >> testlog.txt
+echo $9 >> testlog.txt
+echo ${10}  >> testlog.txt
+echo ${11}  >> testlog.txt
