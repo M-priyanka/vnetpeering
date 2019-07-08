7e4ebb8620c66e11da612b0ec2cff2c1277e9003
M-priyanka <31058016+M-priyanka@users.noreply.github.com>
2019-07-08 16:41:10 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-cd /home/$USER
-echo $1 >> testlog.txt
-echo $2 >> testlog.txt
-echo $3 >> testlog.txt
-echo $4 >> testlog.txt
-echo $5 >> testlog.txt
-echo $6 >> testlog.txt
-echo $7 >> testlog.txt
-echo $8 >> testlog.txt
-echo $9 >> testlog.txt
-echo ${10}  >> testlog.txt
-echo ${11}  >> testlog.txt
+echo $1 >>testlog.txt
+echo $2 >>testlog.txt
+echo $3 >>testlog.txt
+echo $4 >>testlog.txt
+echo $5 >>testlog.txt
+echo $6 >>testlog.txt
+echo $7 >>testlog.txt
+echo $8 >>testlog.txt
+echo $9 >>testlog.txt
+echo ${10} >>testlog.txt
+echo ${11} >>testlog.txt
